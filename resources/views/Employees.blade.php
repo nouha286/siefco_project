6ec9e745d746c7390a5f4e46398ee39ecccdc859
kerkazou <kerkazou.zakaria@gmail.com>
2022-08-18 14:03:36 +0100

    Switch table Active and Desactive

--- resources/views/Employees.blade.php
+++ resources/views/Employees.blade.php
@@ -33,10 +33,9 @@
                 @endif
                 <div class="d-flex flex-row-reverse justify-content-between align-items-center m-4">
                     <div>
-                        <select class="form-select text-center fs-5 fw-bold" style="max-width: 300px; border:none; background-color: var(--second--white-color-color);">
-                            <option value="">المستخدمون</option>
-                            <option value="">المستخدمون الجدد</option>
-                            <option value="">المستخدمون المحذوفين</option>
+                        <select class="form-select text-center fs-5 fw-bold" id="input_select" onchange="selectEmploye()" style="max-width: 300px; border:none; background-color: var(--second--white-color-color);">
+                            <option value="1">المستخدمون</option>
+                            <option value="0">المستخدمون المحذوفين</option>
                         </select>
                     </div>
                     <div class="input-group me-3" style="width: 25%;">
@@ -103,10 +102,13 @@
                     </div>
                     <!-- Modal Edit Emplyee -->
                 </div>
-                <table class="table mb-0 text-center" id="myTable">
+                <table class="table mb-0 text-center" id="table_employe">
                     <thead>
                         <tr>
-                        @if(session('role')=='Admin')<th class="col-1 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>@endif
+                            @if(session('role')=='Admin')
+                                <th class="col-1 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
+                            @endif
+                            <th class="d-none">التفعيل</th>
                             <th class="col-3 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">رقم الهاتف</th>
                             <th class="col-4 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">البريد الالكتروني</th>
                             <th class="col-2 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">النسب</th>
@@ -115,8 +117,8 @@
                     </thead>
                     <tbody>
                         @foreach($Employee as $Employee)
-                            @if( $Employee->Activation==1)
-                                <tr class="item">
+                            {{-- @if( $Employee->Activation==1) --}}
+                                <tr class="item tr_employe">
                                     @if(session('role')=='Admin')
                                         <td class="  col-1 d-flex gap-2">
                                             <form action="{{ route('delete.Employe',$Employee->id) }}" method="post">
@@ -127,99 +129,59 @@
                                             <button type="submit" class="btn btn-edit" style="background-color:white; color:black; border:none;" data-bs-toggle="modal" data-bs-target="#exampleModaledit"><i class="bi bi-pen-fill"></i></button>
                                         </td>
                                     @endif
-                                    <td class="id_devise d-none  col-2 ">{{$Employee->id}}</td>
-                                    <td class="Phone col-3 ">{{$Employee->Number_phone}}</td>
-                                    <td class="Email col-4 ">{{$Employee->Email}}</td>
-                                    <td class="Last_Name col-2 ">{{$Employee->Last_Name}}</td>
+                                    <td class="id_devise d-none col-2">{{$Employee->id}}</td>
+                                    <td class="d-none">{{$Employee->Activation}}</td>
+                                    <td class="Phone col-3">{{$Employee->Number_phone}}</td>
+                                    <td class="Email col-4">{{$Employee->Email}}</td>
+                                    <td class="Last_Name col-2">{{$Employee->Last_Name}}</td>
                                     <td class="First_Name col-2 First_Name">{{$Employee->First_Name}}</td>
                                 </tr>
-                            @endif
+                            {{-- @endif --}}
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
-
-
-
-
-        <!-- Employee supprimé -->
-        @if(session('role')=='Admin')
-        <div class="container-fluid py-4">
-            <div class="card border-0 shadow-sm overflow-auto" style="min-height: 350px; max-height: 350px; border-radius: 16px;">
-                @if (session('success_restore'))
-                <div class="alert alert-success text-center alert-dismissible fade show" role="alert">
-                    {{ session('success_restore') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-                @endif
-                <div class="d-flex flex-row-reverse justify-content-between align-items-center m-4">
-                    <div>
-                       <h4> المستخدمون</h4>
-                    </div>
-                    <div class="input-group me-3" style="width: 25%;">
-                        <input type="text" class="form-control" placeholder="الاسم" style="height: 45px;">
-                        <span class="input-group-text" style="border-radius: 0px 16px 16px 0px;"><i class="bi bi-search"></i></span>
-                    </div>
-                </div>
-                <table class="table mb-0 text-center" id="myTable">
-                    <thead>
-                        <tr>
-                            <th class="col-1 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
-                            <th class="col-3 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">رقم الهاتف</th>
-                            <th class="col-4 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">البريد الالكتروني</th>
-                            <th class="col-2 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">النسب</th>
-                            <th class="col-2 text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">الاسم</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach( $Employee_deleted as $Employee)
-                        @if( $Employee->Activation==0)
-                        <tr class="item">
-                            <td class="  col-1 ">
-                                <form action="{{ route('delete.Employe',$Employee->id) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn" style="border:none; background-color:white;" type="submit"><i class="bi bi-arrow-clockwise"></i></button>
-                                </form>
-                            </td>
-                            <td class="Phone col-3 ">{{$Employee->Number_phone}}</td>
-                            <td class="Email col-4 ">{{$Employee->Email}}</td>
-                            <td class="Last_Name col-2 ">{{$Employee->Last_Name}}</td>
-                            <td class="First_Name col-2 First_Name">{{$Employee->First_Name}}</td>
-                            <td class="id_devise col-2  ">{{$Employee->id}}</td>
-
-                        </tr>
-                        @endif
-                        @endforeach
-
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-        @endif
-
 
 <script>
-            // Update Of Devise
-            document.querySelectorAll('.btn-edit').forEach(function(btn){
-                btn.addEventListener('click',function(event){
-                    let select = event.target.closest('.item');
-                    let id_devise = select.querySelector('.id_devise').innerHTML;
-                    let First_Name = select.querySelector('.First_Name').innerHTML;
-                    let Last_Name = select.querySelector('.Last_Name').innerHTML;
-                    let Email = select.querySelector('.Email').innerHTML;
-                    let Phone = select.querySelector('.Phone').innerHTML;
+    // Select Employee
+    selectEmploye();
+    function selectEmploye() {
+        var input_select, table_employe, tr, td, i, txtValue;
+        input_select = document.getElementById("input_select");
+        filter = input_select.value;
+        table_employe = document.getElementById("table_employe");
+        tr = table_employe.querySelectorAll('.tr_employe');
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+    // Update Of Employee
+    document.querySelectorAll('.btn-edit').forEach(function(btn){
+        btn.addEventListener('click',function(event){
+            let select = event.target.closest('.item');
+            let id_devise = select.querySelector('.id_devise').innerHTML;
+            let First_Name = select.querySelector('.First_Name').innerHTML;
+            let Last_Name = select.querySelector('.Last_Name').innerHTML;
+            let Email = select.querySelector('.Email').innerHTML;
+            let Phone = select.querySelector('.Phone').innerHTML;
 
-                    document.querySelector('#exampleModaledit .id_devise').value = id_devise;
-                    document.querySelector('#exampleModaledit .First_Name').value = First_Name;
-                    document.querySelector('#exampleModaledit .Last_Name').value = Last_Name;
-                    document.querySelector('#exampleModaledit .Email').value = Email;
-                    document.querySelector('#exampleModaledit .Phone').value = Phone;
-                })
-            })
-        </script>
+            document.querySelector('#exampleModaledit .id_devise').value = id_devise;
+            document.querySelector('#exampleModaledit .First_Name').value = First_Name;
+            document.querySelector('#exampleModaledit .Last_Name').value = Last_Name;
+            document.querySelector('#exampleModaledit .Email').value = Email;
+            document.querySelector('#exampleModaledit .Phone').value = Phone;
+        })
+    })
+</script>
 
         <!-- Copyright -->
         <div class="position-fixed bottom-0 start-50 text-center h6">Copyright &copy; SayfCo 2022</div>
