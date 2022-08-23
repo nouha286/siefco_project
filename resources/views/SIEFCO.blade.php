a41b8653d6954a15110cb69c103218307caf77bb
kerkazou <kerkazou.zakaria@gmail.com>
2022-08-23 13:19:40 +0100

    ....

--- resources/views/SIEFCO.blade.php
+++ resources/views/SIEFCO.blade.php
@@ -13,6 +13,7 @@
     <!--  -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    <link rel="shortcut icon" href="http://localhost/siefco_project/public/assets/logo.png"/>
     <title>SIEFCO</title>
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::delete('operation_commercial/{id}','OperationCommercialController@deleteO
 
 // Generate Pdf
 // Route::get('/SIEFCO', 'PDFController@index');
-Route::get('generatePDF', 'PDFController@generatePDF');
+Route::get('generatePDF', 'PDFController@index');
 
 
 
