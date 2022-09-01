c7ca5a8f5dbbc559386cadb7b75a519ce0415c8a
kerkazou <kerkazou.zakaria@gmail.com>
2022-09-01 17:59:46 +0100

    Forget_password

--- resources/views/Forget_password.blade.php
+++ resources/views/Forget_password.blade.php
@@ -37,29 +37,24 @@
                 <div class="d-flex flex-column justify-content-center gap-2"
                     style="width: 80%; height: 85vh; margin-left: 10%;">
                     <h1 class="text-center">{{ __('استعادة القن السري') }}</h1>
-                    <p class="text-center" id="error_signin"> {{ __('ادخل بريدك الالكتروني ') }} </p>
-                    <p id="error_signin"></p>
-                    <form class="d-flex flex-column gap-2" method="POST" action="{{ route('ifissetemail') }}"
-                        id="form_signin">
+                    <p class="text-center" id="error_email"> {{ __('ادخل بريدك الالكتروني ') }} </p>
+                    <form class="d-flex flex-column gap-2" method="POST" action="{{ route('ifissetemail') }}" id="form">
                         @csrf
                         @if (session('error'))
                             <div class="alert alert-danger text-center alert-dismissible fade show" role="alert">
                                 {{ session('error') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
                         @endif
                         @if (session('success'))
                             <div class="alert alert-success text-center alert-dismissible fade show" role="alert">
                                 {{ session('success') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
                         @endif
 
-                        <input type="text" name="email" id="email_signin"
+                        <input type="text" name="email" id="email"
                             placeholder="{{ __('البريد الالكتروني') }}" class="border-0 col-form-label">
-                        <p class="text-danger float-end me-4" id="error_email"></p>
 
                         <input type="submit" name="signin" value="{{ __('تحقق') }} ">
                     </form>
@@ -74,4 +69,23 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
+<script>
+    // Validation Form Sign In
+    const form = document.getElementById('form');
+    const email = document.getElementById('email');
+    const error_email = document.getElementById('error_email');
+    const pattern_email = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    form.addEventListener('submit', (e) => {
+        if (email.value == "") {
+            e.preventDefault();
+            error_email.innerHTML = '<p class="text-center text-danger" id="error_email"> {{ __("ادخل بريدك الالكتروني ") }} </p>';
+        }else if(!pattern_email.test(email.value)) {
+            e.preventDefault();
+            error_email.innerHTML = '<p class="text-center text-danger" id="error_email"> {{ __("البريد الإلكتروني غير صالح") }} </p>';
+        }else{
+            rror_email.innerHTML = '<p class="text-center" id="error_email"> {{ __("ادخل بريدك الالكتروني ") }} </p>';
+        }
+    });
 </script>
